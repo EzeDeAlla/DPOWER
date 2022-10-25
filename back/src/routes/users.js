97058956f9a1be7e3c2708ec9d06534945a88d26
IgnacioVivas <ignacab@hotmail.com>
2022-10-25 20:20:58 -0300

    post user

--- back/src/routes/users.js
+++ back/src/routes/users.js
@@ -1,8 +1,28 @@
 const { Router } = require('express');
 const router = Router();
+const { User } = require('../db');
 
-
-
-
+router.post('', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.findOne({
+      where: { email },
+    });
+    if (name && email && password) {
+      if (!user) {
+        const newUser = await User.create({
+          name,
+          email,
+          password,
+        });
+        res.status(200).send(newUser);
+      } else {
+        throw new Error('user already exists');
+      }
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 module.exports = router;
