6c05dd12f4cf75951f2aaba71051e9e555fa49f2
Nosrey <106562225+Nosrey@users.noreply.github.com>
2022-10-26 20:47:55 -0400

    agregada la funcion de modificar poweres por la routa /post de tipo put

--- back/src/routes/post.js
+++ back/src/routes/post.js
@@ -1,8 +1,30 @@
 const { Router } = require('express');
 const router = Router();
+const { UserInfo } = require('../db')
 
 
 
+router.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { powers } = req.query;
+
+        let user = await UserInfo.update(
+            {
+                powers: powers,
+            },
+            {
+                where: {
+                    id: id
+                },
+            }
+        );
+        res.send(user)
+    }
+    catch {
+        return null
+    }
+})
 
 
 module.exports = router;
