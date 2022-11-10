1e0a8ff2bac576502daa5482f98f3272520b46f8
Ezequiel Martinez <ezeelmartinez@gmail.com>
2022-11-10 18:23:29 -0300

    funciona xfavor

--- back/src/routes/post.js
+++ back/src/routes/post.js
@@ -29,7 +29,7 @@ router.get('/:id', async (req, res) => {
 
     {
       model: UserInfo,
-      attributes: ['name'],
+      attributes: ['name', 'validate'],
       where: {
         id: idUser,
       },
