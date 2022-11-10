cd5bb22bc67835ff37b283fd604157927f35a969
Ezequiel Martinez <ezeelmartinez@gmail.com>
2022-11-10 16:31:51 -0300

    aaa

--- back/src/routes/post.js
+++ back/src/routes/post.js
@@ -13,7 +13,7 @@ router.get('', async (req, res) => {
   }
 });
  // asdasdasdasasdas
-// || POST/:ID || //
+// || POST/:ID || // asdas das
 router.get('/:id', async (req, res) => {
   const id = req.params.id;
   const post = await Post.findByPk(id);
