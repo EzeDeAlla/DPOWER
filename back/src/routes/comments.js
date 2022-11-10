ca05337c85074beadac4092b8ec360291618540a
Ezequiel Martinez <ezeelmartinez@gmail.com>
2022-11-10 17:08:58 -0300

    funciona xfavor

--- back/src/routes/comments.js
+++ back/src/routes/comments.js
@@ -7,7 +7,7 @@ const { Comment } = require('../db');
 // || GET /COMMENTS || // asd
 router.get('', async (req, res) => {
   try{
-    const comments = dbComments();
+    const comments = await dbComments();
     res.status(200).send(comments)
   } catch (error) {
     res.status(400).send(error);
