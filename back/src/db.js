ef94e415e31882db3cb8d890ca704f3f3973540b
IgnacioVivas <ignacab@hotmail.com>
2022-11-16 01:49:56 -0300

    actualizando

--- back/src/db.js
+++ back/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const { userInfo } = require('os');
-// const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+// const { DB_USER, gitDB_PASSWORD, DB_HOST } = process.env;
 
 const { PGHOST, PGUSER, PGPASSWORD, PGPORT } = process.env;
 
